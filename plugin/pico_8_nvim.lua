9828d5aaec04d029d4a9d73b41f7f8dad2664abe
jeznon <jesadamer@gmail.com>
2025-10-26 21:48:17 -0600

    feat: pico 8 plugin first version

--- plugin/pico_8_nvim.lua
+++ plugin/pico_8_nvim.lua
@@ -1,8 +1,18 @@
-if vim.g.loaded_pico8_connect then return end
+-- plugin/pico8_connect.lua
+if vim.g.loaded_pico8_connect then
+  return
+end
 vim.g.loaded_pico8_connect = true
 
-vim.api.nvim_create_user_command('Pico8Hello', function()
-  require('pico_8_nvim').hello()
+vim.api.nvim_create_user_command("Pico8Pick", function()
+  require("pico8_connect").pick_cartridge()
 end, {})
 
+vim.api.nvim_create_user_command("Pico8Inject", function()
+  require("pico8_connect").inject_cartridge_code()
+end, {})
+
+vim.api.nvim_create_user_command("Pico8SetCartsDir", function()
+  require("pico8_connect").set_carts_dir()
+end, {})
 
